019a8082c28fb93d4a9db5f262710397d1a414e4
Marco V. Aguilar Soto <aguilarsoto@gmail.com>
2012-05-07 13:04:41 -0600

    dummy tests for rails admin fail on edit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DummyTest::Application.config.secret_token = 'd0099ba7ce3ee4ba8fb8407dae8456bf9baaaed2879f0cda6c98e56b7c4e583ebc8569712c291d84a4698c7fd89405a9df17e630b3c6643fd8eafab5826bd1f0'
